eed4d5eddf03628393f57f2129729d21593cbc8a
Nanda Pramudia Santosa <65044968+nanda-672@users.noreply.github.com>
2025-11-28 09:40:18 +0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (86.58%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,16 +1,5 @@
 class Solution:
-    # memo = {}
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # if len(cost) == 2:
-        #     return min(cost[0],cost[1])
-        # elif len(cost) < 2:
-        #     return 0
-        # # elif len(cost) in self.memo:
-        # #     return self.memo[len(cost)]
-        # answer = self.minIgnoreNone(self.minCostClimbingStairs(cost[:-1])+cost[-1], self.minCostClimbingStairs(cost[:-2])+cost[-2])
-        # # self.memo[len(cost)] = answer
-        # return answer
-
         dp = [0] * len(cost)
         dp[-1] = cost[-1]
         dp[-2] = cost[-2]

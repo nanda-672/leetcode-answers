6fd9aee2ff701c77b2e694a10218e675be73b41f
Nanda Pramudia Santosa <65044968+nanda-672@users.noreply.github.com>
2025-11-28 10:24:47 +0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (61.71%) - LeetSync

--- 1086-divisor-game/divisor-game.py
+++ 1086-divisor-game/divisor-game.py
@@ -1,7 +1,4 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        if n%2 == 0:
-            return True
-        else:
-            return False
+        return n%2 == 0
         

37afb09443410a3bb73435be800b1aeb9c62f198
Nanda Pramudia Santosa <65044968+nanda-672@users.noreply.github.com>
2025-12-08 17:05:24 +0700

    Time: 1669 ms (20.83%) | Memory: 18.6 MB (75.05%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -3,7 +3,6 @@ class Solution:
         jump_table = [100000] * len(nums)
         jump_table[-1] = 0
         n = len(nums)
-        
         for i in range(n-2, -1, -1):
             if nums[i] + i >= n-1:
                 jump_table[i] = 1
@@ -13,7 +12,7 @@ class Solution:
                     if i + j < n:
                         if jump_table[i+j] + 1 < min_jump:
                             min_jump = jump_table[i+j] + 1
-                jump_table[i] = min_jump
+                jump_table[i] = min_jump              
         
         return jump_table[0]
 
